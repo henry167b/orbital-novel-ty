670fc21e06264afdd0bc62293f4a0ba3ea327770
Ibnu Taimiyyah <ibnu2651@gmail.com>
2023-05-20 01:15:19 +0800

    Add login page

--- orbital-app/App.js
+++ orbital-app/App.js
@@ -1,20 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Surface, Text, TextInput, Checkbox, Button, PaperProvider } from 'react-native-paper';
+
+function UsernameEntry() {
+  const [text, setText] = useState("");
+
+  return (
+    <View style={{width: '100%'}}>
+      <Text variant='titleMedium' style={{alignItems: 'flex-start'}}>myLibrary User</Text>
+      <TextInput
+        mode='outlined'
+        value={text}
+        onChangeText={setText}
+        style={styles.textInput}
+      />
+    </View>
+  );
+}
+
+function PasswordEntry() {
+  const [text, setText] = useState("");
+
+  return (
+    <View style={{width: '100%'}}>
+      <Text variant='titleMedium' style={{alignItems: 'flex-start'}}>Password</Text>
+      <TextInput
+        mode='outlined'
+        style={styles.textInput}
+      />
+    </View>
+  );
+}
+
+function LoginButton() {
+  return (
+    <Button mode='contained-tonal' contentStyle={styles.loginButton}>
+      Sign In
+    </Button>
+  );
+}
+
+function RememberMe() {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <View style={{flexDirection: 'row', alignItems: 'baseline', justifyContent: 'flex-start'}}>
+      <Checkbox.Android
+        status={checked ? 'checked' : 'unchecked'}
+        onPress={() => {setChecked(!checked)}}
+
+      />
+      <Text variant='bodyLarge'>Remember me</Text>
+    </View>
+    
+  );
+}
+
+function LoginContainer() {
+
+  return (
+    <Surface style={styles.loginContainer} elevation={3}>
+      <Text variant='headlineSmall' style={{paddingVertical: 10}}>Welcome Back!</Text>
+      <UsernameEntry />
+      <PasswordEntry />
+      <LoginButton />
+      <RememberMe />
+    </Surface>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text variant='displayMedium' style={{padding: 50}}>Novel-ty</Text>
+        <LoginContainer />
+      </SafeAreaView>
+    </SafeAreaProvider>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#BFBFBF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
+    paddingTop: 25,
+    paddingBottom: 25
   },
+  loginContainer: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    padding: 30,
+    gap: 20,
+    width: '75%',
+    borderRadius: 10
+  },
+  textInput: {
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingHorizontal: 1
+  },
+  loginButton: {
+    width: '100%'
+  }
 });
