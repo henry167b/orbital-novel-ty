1d7c3e4964e12af1aec2f3513b1c222c8846173b
Ibnu Taimiyyah <ibnu2651@gmail.com>
2023-07-04 16:43:48 +0800

    Change icons to use relative path

--- orbital-app/app/(home)/_layout.jsx
+++ orbital-app/app/(home)/_layout.jsx
@@ -2,10 +2,10 @@ import { Tabs } from "expo-router";
 import { Image } from "react-native";
 
 
-const HomeIcon = require("/Users/henry/Documents/GitHub/orbital-novel-ty/orbital-app/assets/home_4991416.png");
-const BookIcon = require("/Users/henry/Documents/GitHub/orbital-novel-ty/orbital-app/assets/notebook_3311424.png");
-const SearchIcon = require("/Users/henry/Documents/GitHub/orbital-novel-ty/orbital-app/assets/search_5186446.png");
-const LibrariesIcon = require("/Users/henry/Documents/GitHub/orbital-novel-ty/orbital-app/assets/placeholder_684809.png")
+const HomeIcon = require("../../assets/home_4991416.png");
+const BookIcon = require("../../assets/notebook_3311424.png");
+const SearchIcon = require("../../assets/search_5186446.png");
+const LibrariesIcon = require("../../assets/placeholder_684809.png")
 
 export const unstable_settings = {
   initialRouteName: "index",
