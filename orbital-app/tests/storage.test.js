65b1ee42f834d5e8a9c8d805bd955213ee1ed13e
Ibnu Taimiyyah <ibnu2651@gmail.com>
2023-07-19 01:11:19 +0800

    Add Storage test cases

--- orbital-app/tests/storage.test.js
+++ orbital-app/tests/storage.test.js
@@ -173,3 +173,103 @@ describe('removeBook Function Test', () => {
     expect(JSON.parse(mockLibraries)[0].books).toHaveLength(0);
   });
 });
+
+describe('getBooks Function Test', () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // Reset all mock functions after each test case
+  });
+
+  it('should return an empty array when no books are stored', async () => {
+    AsyncStorage.getItem.mockResolvedValueOnce(JSON.stringify([]));
+
+    const result = await storage.getBooks();
+
+    expect(result).toEqual([]);
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('@books');
+  });
+
+  it('should return an array of books when books are stored', async () => {
+    const mockBooks = [
+      { isbn: 'ISBN 1', title: 'Book 1', author: 'Author 1', bid: 'BID1' },
+      { isbn: 'ISBN 2', title: 'Book 2', author: 'Author 2', bid: 'BID2' },
+    ];
+
+    AsyncStorage.getItem.mockResolvedValueOnce(JSON.stringify(mockBooks));
+
+    const result = await storage.getBooks();
+
+    expect(result).toEqual(mockBooks);
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('@books');
+  });
+});
+
+describe('getLibraries Function Test', () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // Reset all mock functions after each test case
+  });
+
+  it('should return an array of libraries when libraries are stored', async () => {
+    const mockLibraries = [
+      { name: 'Library1', location: 'Location1', books: [] },
+      { name: 'Library2', location: 'Location2', books: [] },
+    ];
+
+    const result = await storage.getLibraries();
+
+    expect(result).toEqual(mockLibraries);
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('@libraries');
+  });
+});
+
+describe('addRecentSearch Function Test', () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // Reset all mock functions after each test case
+  });
+
+  it('should add a new search query to recent searches', async () => {
+    const mockSearchQuery = 'Test Search Query';
+    const mockRecentSearches = [ 'Previous Search 1', 'Previous Search 2' ];
+
+    AsyncStorage.getItem.mockResolvedValueOnce(JSON.stringify(mockRecentSearches));
+    AsyncStorage.setItem.mockResolvedValueOnce();
+
+    await storage.addRecentSearch(mockSearchQuery);
+
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('@recentSearches');
+
+    expect(AsyncStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith('@recentSearches', JSON.stringify([mockSearchQuery, ...mockRecentSearches]));
+  });
+});
+
+describe('getRecentSearches Function Test', () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // Reset all mock functions after each test case
+  });
+
+  it('should return an empty array when no recent searches are stored', async () => {
+    AsyncStorage.getItem.mockResolvedValueOnce(JSON.stringify([]));
+
+    const result = await storage.getRecentSearches();
+
+    expect(result).toEqual([]);
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('@recentSearches');
+  });
+
+  it('should return an array of recent searches when recent searches are stored', async () => {
+    const mockRecentSearches = [ 'Search Query 1', 'Search Query 2', 'Search Query 3' ];
+
+    AsyncStorage.getItem.mockResolvedValueOnce(JSON.stringify(mockRecentSearches));
+
+    const result = await storage.getRecentSearches();
+
+    expect(result).toEqual(mockRecentSearches);
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('@recentSearches');
+  });
+});
